51508632449cc4dc75dbc93ee5e84b4264b19778
Björn Wilmsmann <bjoern@bjoernkw.com>
2025-02-09 15:53:24 +0100

    Angular update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,10 @@ import { Component, NgZone } from '@angular/core';
 declare var Mite: any;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    standalone: false
 })
 export class HomeComponent {
 
